44096a659dc7a95b604c8f0a15ac9477eb39451f
r0hara <rohara.engr@gmail.com>
2025-02-23 00:42:17 -0500

    mobile check

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function onSubscribe(button) {
-    let inputText = document.getElementsByTagName("input").item(0).value;
+    let input = document.getElementsByTagName("input").item(0)
+    let inputText = input.value;
         
     if (inputText === undefined || !inputText.includes("@")) {
-        button.innerHTML = "Invalid Email"
+        button.innerHTML = "Invalid"
         setTimeout(function(){
             button.innerHTML = "Subscribe";
         }, 1000);
     }
     else {
         button.innerHTML = "Thank you!";
+        input.value = "";
         button.disabled = true;
+        button.style.backgroundColor = 'white';
+        button.style.color = 'rgb(86, 102, 29)';
     }
 }
